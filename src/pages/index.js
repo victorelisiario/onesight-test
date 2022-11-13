0397552e81dfe798bdfb761e45066a538f79429c
Victor Elisiario <victorelisiario2@hotmail.com>
2022-11-13 09:49:50 -0300

    Sidebar deleted

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
-import { Flex, Icon, Stack, Button, HStack, Divider } from '@chakra-ui/react'
+import { Flex, Icon, Stack, Button, HStack } from '@chakra-ui/react'
 import { FaRegTrashAlt } from "react-icons/fa";
 
 import { Header } from '../components/Header'
-import { Sidebar } from '../components/sidebar'
 import { CandidatesTable } from '../components/CandidatesTable';
 import { NewCandidateModal } from '../components/NewCandidateModal';
 
@@ -26,13 +25,11 @@ export default function Home() {
     <>
       <Header />
       <Flex direction="coloumn" justify="center" >
-        <Sidebar />
         <Flex
           justify='center'
           w="100%"
           maxW='1200px'
         >
-          <Divider orientation='vertical' color="red" h="90vh" />
           <Stack spacing='2' w="100%" px="4" maxW='1200px'>
             <Flex w="100%" px="4" maxW='1200px' justify="start">
               <HStack spacing="2" >
